f0aacbe9f5373d507644814617f6df3fdb7522ac
Aleksey Berezka <a.berezka@dodopizza.com>
2024-01-11 01:51:08 +0500

    Separated Lock (#10)

--- Sources/DBThreadSafe/DBThreadSafeContainer.swift
+++ Sources/DBThreadSafe/DBThreadSafeContainer.swift
@@ -1,50 +1,45 @@
 import Foundation
 
-public class DBThreadSafeContainer<T> {
+public final class DBThreadSafeContainer<T> {
     private var value: T
-    private var lock = pthread_rwlock_t()
+    private var lock = Lock()
     
     public init(_ value: T) {
         self.value = value
-        pthread_rwlock_init(&lock, nil)
     }
     
     /// Reads the value stored
     /// - Returns: The value stored in the container.
     public func read() -> T {
-        pthread_rwlock_rdlock(&lock)
-        defer { pthread_rwlock_unlock(&lock) }
+        lock.readLock()
+        defer { lock.unlock() }
         return value
     }
     
     public func read(_ closure: (_ value: T) throws -> Void) rethrows {
-        pthread_rwlock_rdlock(&lock)
-        defer { pthread_rwlock_unlock(&lock) }
+        lock.readLock()
+        defer { lock.unlock() }
         try closure(value)
     }
     
     public func read<U>(_ closure: (_ value: T) throws -> U) rethrows -> U {
-        pthread_rwlock_rdlock(&lock)
-        defer { pthread_rwlock_unlock(&lock) }
+        lock.readLock()
+        defer { lock.unlock() }
         return try closure(value)
     }
     
     /// Replaces current value with a new one
     /// - Parameter newValue: The new value to be stored in the container.
     public func write(_ newValue: T) {
-        pthread_rwlock_wrlock(&lock)
-        defer { pthread_rwlock_unlock(&lock) }
+        lock.writeLock()
+        defer { lock.unlock() }
         value = newValue
     }
         
     /// Returns current value in a closure with possibility to make multiple modifications of any kind inside a single lock.
     public func write(_ closure: (_ value: inout T) throws -> Void) rethrows {
-        pthread_rwlock_wrlock(&lock)
-        defer { pthread_rwlock_unlock(&lock) }
+        lock.writeLock()
+        defer { lock.unlock() }
         try closure(&value)
-    }
-    
-    deinit {
-        pthread_rwlock_destroy(&lock)
     }
 }
