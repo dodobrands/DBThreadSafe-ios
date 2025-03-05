4c2e92df1525b66bd5a398674a59f3fd8bf92114
Aleksey Berezka <a.berezka@dodobrands.io>
2025-03-05 13:42:37 +0500

    Downgrade swift-version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,6 +19,5 @@ let package = Package(
         .testTarget(
             name: "DBThreadSafeTests",
             dependencies: ["DBThreadSafe"]),
-    ],
-    swiftLanguageModes: [.v6]
+    ]
 )
