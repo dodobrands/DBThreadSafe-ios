fbc7bd1fbbfc14d85c55a9ee9f27256429e20cb3
Anton Lagutin <a.lagutin@dodobrands.io>
2025-03-05 17:46:42 +1000

    UnsafeMutablePointer in Lock. (#11)

--- Sources/DBThreadSafe/Lock.swift
+++ Sources/DBThreadSafe/Lock.swift
@@ -1,25 +1,27 @@
 import Foundation
 
 final class Lock {
-    private var lock = pthread_rwlock_t()
+    private let lock = UnsafeMutablePointer<pthread_rwlock_t>.allocate(capacity: 1)
     
     init() {
-        precondition(pthread_rwlock_init(&lock, nil) == 0, "Failed to initialize the lock")
+        precondition(pthread_rwlock_init(lock, nil) == 0, "Failed to initialize the lock")
     }
     
     func readLock() {
-        pthread_rwlock_rdlock(&lock)
+        pthread_rwlock_rdlock(lock)
     }
     
     func writeLock() {
-        pthread_rwlock_wrlock(&lock)
+        pthread_rwlock_wrlock(lock)
     }
     
     func unlock() {
-        pthread_rwlock_unlock(&lock)
+        pthread_rwlock_unlock(lock)
     }
     
     deinit {
-        precondition(pthread_rwlock_destroy(&lock) == 0, "Failed to destroy the lock")
+        precondition(pthread_rwlock_destroy(lock) == 0, "Failed to destroy the lock")
+        
+        lock.deallocate()
     }
 }
